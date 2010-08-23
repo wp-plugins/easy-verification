e5b9d484b117b833398479a5e7a218a83a2c9c33
collinsinternet <collinsinternet@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-08-23 16:35:27 +0000

    Version 1.3 - Added Bing Support.
    git-svn-id: https://plugins.svn.wordpress.org/easy-verification/tags/1.2@279811 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ easyverification.php
@@ -0,0 +1,68 @@
+<?php
+/*
+Plugin Name: Easy Verification
+Plugin URI: http://www.allancollins.net/80/easy-verification/
+Description: This plugin will allow you to easily verify your site for Google and Yahoo!.
+Author: Allan Collins
+Version: 1.2.2
+Author URI: http://www.allancollins.net
+*/
+/*
+Copyright (C) 2008 Allan Collins
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+// Hook for adding admin menus
+add_action('admin_menu', 'easyv_add_pages');
+
+// action function for above hook
+function easyv_add_pages() {
+    add_options_page('Easy Verification', 'Easy Verification', 8, 'easyverification', 'easyv_options');
+}
+
+function easyv_options() {
+    echo "<div style=\"margin-left:200px; \">";
+    echo "<h2>Easy Verification</h2>";
+    echo "Enter the verification keys for Google Webmaster Tools and Yahoo! Site Explorer.<br/><br/>";
+    echo '<form method="post" action="options.php">';
+    wp_nonce_field('update-options');
+    echo 'Example: &lt;meta name="verify-v1" content="<b>URPaftUcC5k1lmSXeVBn3JpqyPJIMXDhPomojAIOhWE=</b>" /&gt;<br/><br/>';
+    echo 'Google: <input type="text" name="easyv_google" value="' . get_option('easyv_google') . '" /><br/><br/>';
+    echo 'Example: &lt;META name="y_key" content="<b>5ea13d6e265bbf4c</b>" &gt;<br/><br/>';
+    echo 'Yahoo!: <input type="text" name="easyv_yahoo" value="' . get_option('easyv_yahoo') . '" /><br/><br/>';
+    echo '<input type="hidden" name="action" value="update" />';
+    echo '<input type="hidden" name="page_options" value="easyv_google,easyv_yahoo" />';
+    echo '<p class="submit">
+<input type="submit" name="Submit" value="Save Changes" />
+</p>';
+    echo "</div>";
+}
+
+add_action('wp_head', 'easyv_add_meta');
+
+
+function easyv_add_meta() {
+
+    echo "<!-- Easy Verification -->\n";
+    echo '<meta name="verify-v1" content="'. get_option('easyv_google') . '" />';
+    echo '<meta name="google-site-verification" content="'. get_option('easyv_google') . '" />';
+    echo '<meta name="y_key" content="'. get_option('easyv_yahoo') . '" />';
+    echo "<!-- End Easy Verification -->\n";
+
+
+}
+
+
+
+?>
